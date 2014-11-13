efc42b8ba1865a4bc51c33c21695f584489d39bb
gfontani <gabriella@fontani.net>
2014-11-13 11:20:58 -0700

    added termIndex and HashTable
    added complete TermIndex interface, added incomplete HashTable class

---
+++ src/HashTable.java
@@ -0,0 +1,20 @@
+
+public class HashTable implements TermIndex{
+	private int size;
+	
+	
+	public void add(String filename, String newWord){
+		
+	}
+	public int size(){
+		return this.size;
+	}
+	public void delete(String word){
+		
+	}
+	public Term get(String word, Boolean printP){
+		Term temp = new Term(word);
+		return temp;
+	}
+
+}
