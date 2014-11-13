d421caa94be623ea08d1733a61fc18cab5068a81
gfontani <gabriella@fontani.net>
2014-11-13 11:49:56 -0700

    working on hash table
    created constructor method and hash code method, started add
    method...kinda

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,14 +1,26 @@
 
 public class HashTable implements TermIndex{
+	//size keeps track of how many terms are in the hash table
 	private int size;
+	//termTable is the hash table where terms are stored
 	Term[] termTable;
+	//hash size is the size of the array 
+	private int hashSize;
 	
-	
-	public void HashTable(int sizeOfArray){
-		termTable = new Term[sizeOfArray];
+	//constructor creates termTable to be the correct size
+	public void HashTable(int hashSize){
+		termTable = new Term[hashSize];
 		size = 0;
+		this.hashSize = hashSize;
 	}
+	//adds a term to the hash table
+	//expands size of table and rehashes when 80% full
 	public void add(String filename, String newWord){
+		int hashCode = hashCode(newWord);
+
+		
+		
+		
 		
 	}
 	public int size(){
@@ -20,6 +32,12 @@ public class HashTable implements TermIndex{
 	public Term get(String word, Boolean printP){
 		Term temp = new Term(word);
 		return temp;
+	}
+	
+	public int hashCode(String word){
+		int intCode = word.toLowerCase().hashCode();
+		intCode = intCode%hashSize;
+		return intCode;
 	}
 
 }
